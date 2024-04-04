7b726f194ade4d22905980c9982583eb684ca5ca
Hao Nguyen <ext-hao.nguyen@spdigital.sg>
2024-04-04 17:15:50 +0700

    change api content

--- project/main.go
+++ project/main.go
@@ -16,7 +16,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, Docker! <3. This is a Test API")
+		return c.HTML(http.StatusOK, "Hello, Docker! <3")
 	})
 
 	e.GET("/health", func(c echo.Context) error {
